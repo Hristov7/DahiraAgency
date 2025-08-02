7bd355df01a3e2883906c6b1486a013259d6b7c9
Hristov7 <danielantonov07@gmail.com>
2025-08-02 19:15:59 +0300

    Resolve no IEmailSender developer exception page issue.

---
+++ Data/NoOpEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace DahiraAgency.Data
+{
+    public class NoOpEmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // For development, just log or do nothing
+            return Task.CompletedTask;
+        }
+    }
+}
